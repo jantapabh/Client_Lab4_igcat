448f237233d45c6c3775bb4dca29d4c29394a138
s6035512034@phuket.psu.ac.th <jantapa2407@gmail.com>
2020-02-06 10:38:08 +0700

    axios test

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Icon from './components/Icon';
+import axios from 'axios';
 
+// axios เป็น module mี่ใช้ในการ เet api
+
+
+// const fetchData = async () => {
+
+ 
+//   const fetchData = await axios.get(URL);
+//   const respone =  await fetchData.json();
+
+//   console.log(respone);
+
+// } 
 
 function App() {
 
-  let [counts, setCounts] = useState([0,0,0]);
-  
-  // let handleClick = () => {
 
-  //   setCount(count + 1)
+  const getWeather = async () => {
+
+   await  axios.get(url);
+
+  }
+
+  const url ='http://api.openweathermap.org/data/2.5/weather?q=83000,th&units=metric&appid=fd68c0f2039c5a25f666a9ff374bc93e';
+
+
+   useEffect(() => {
+
+   getWeather()
+
+    
+   }, []);
+
+
+
+  // let [counts, setCounts] = useState([0,0,0]);
+  
+  // let handleClick = (index) => {
+
+  //  // edit word
+  //   counts[index]++;
+
+
+  //   //update state
+  //   setCounts([...counts])
 
   // }
 
-  //ถ้าต้องการส่ง handleClick ไปยัง icon ได้
+  // //ถ้าต้องการส่ง handleClick ไปยัง icon ได้
 
   return (
     <div className="App">
-    {
+        
+     <Icon />
 
-      counts.map((count,index) => {
-
-        return  <Icon key={index} count={count} />
-
-      }) 
-    }
-     {/* {
-       [
-         <Icon counts={counts[0]} />,
-         <Icon counts={counts[1]} />,
-         <Icon counts={counts[2]} />
-       ]
-     } */}
     </div>
   );
 }
