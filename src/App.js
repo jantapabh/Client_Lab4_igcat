dc7356ef6be28bff3ab829a3855038bf133e1526
s6035512034@phuket.psu.ac.th <jantapa2407@gmail.com>
2020-02-06 11:12:09 +0700

    axios add api tempature and humidity

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import axios from 'axios';
 
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=83000,th&units=metric&appid=fd68c0f2039c5a25f666a9ff374bc93e';
 
-let [temp, setTemp] = useState(0)
-let [humi, setHumi] = useState(0)
+
 
 
 
 function App() {
 
+ 
+  let [temp, setTemp] = useState(0)
+  let [humi, setHumi] = useState(0)
+
+
+
   const getWeather = async () => {
+  
 
     let {data} = await axios.get(url);
-
+  
     setTemp(data.main.temp);
-    setHumi(data.main.humi);
-
+    setHumi(data.main.humidity);
+  
   }
 
   useEffect(() => {
@@ -29,16 +35,11 @@ function App() {
   }, []);
 
 
-
-
   return (
-
-
     <div className="App">
     <Icon />
     <div>Tem : {temp}</div>
-
-
+    <div>Humi : {humi}</div>
     </div>
   );
 }
