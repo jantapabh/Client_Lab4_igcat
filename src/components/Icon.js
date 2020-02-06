e4cce7ec0219674f4ee816b58f9de1e0d9df4bd5
s6035512034@phuket.psu.ac.th <jantapa2407@gmail.com>
2020-02-06 09:48:47 +0700

    add state

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,16 +4,18 @@ import React, { useState } from 'react';
 // props คือการส่งจาก components หนึ่งไปยังอีก components
 
 const Icon = props => {
-    let count = 0;
-    let handleClick = () => {
-        count = count + 1;
-        console.log(count);
-    }
+
+    let [count, setCount] = useState(1);
+
+    // let handleClick = () => {
+    //     count = count + 1;
+    //     console.log(count);
+    // }
 
     return (
         <div>
             <p>Count : {count}</p>
-            <button onClick={handleClick}>add</button>
+            <button onClick={() => setCount(count+1)}>add</button>
         </div>
     )
 
